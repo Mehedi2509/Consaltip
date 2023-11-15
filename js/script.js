8c3c417b972f08bfedf54585eeb1bacbc88848d1
MD MEHEDI HASAN PARVEZ <mehedi25092@gmail.com>
2023-11-15 18:39:49 +0600

    added preloader section

--- js/script.js
+++ js/script.js
@@ -1,251 +1,278 @@
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
 
 
-// Hero section //
-
-$('.hero-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 2000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                dots: false,
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
+        // preloader //
+        $(window).on("load", function () {
+            $("#preloader").css({"visibility": "hidden"});
+            $("#preloader-status").delay(200).css({"visibility": "hidden"});
+            $("body").delay(200).css({
+                "overflow-x": "hidden"
+            });
+        });
 
 
+        // Mobile toggle button
+        $(".mobile-menu-toggle-btn").on("click", function () {
+            $(".sidebar-menu-area").addClass("active");
+            $(".body-overlay").addClass("active");
+        });
+        $(".menu-sidebar-close-btn").on("click", function () {
+            $(".sidebar-menu-area").removeClass("active");
+            $(".body-overlay").removeClass("active");
+        });
 
-// Services slider sections //
-$('.services-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
+
+        // Hero section //
+
+        $('.hero-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        dots: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
 
 
 
-
-// Case studies section //
-
-$('.case-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
-
-$('.case-prev-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.case-slider .slick-prev').trigger('click');
-});
-$('.case-next-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.case-slider .slick-next').trigger('click');
-});
-
-
-
-// Testimonial Section //
-
-$('.testimonial-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
-
-$('.testimonial-prev-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.testimonial-slider .slick-prev').trigger('click');
-});
-$('.testimonial-next-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.testimonial-slider .slick-next').trigger('click');
-});
+        // Services slider sections //
+        $('.services-slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
 
 
 
 
-// Team sections //
+        // Case studies section //
 
-$('.team-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
+        $('.case-slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
 
-$('.team-prev-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.team-slider .slick-prev').trigger('click');
-});
-$('.team-next-btn').on('click', function (e) {
-    e.preventDefault();
-    $('.team-slider .slick-next').trigger('click');
-});
+        $('.case-prev-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.case-slider .slick-prev').trigger('click');
+        });
+        $('.case-next-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.case-slider .slick-next').trigger('click');
+        });
 
 
-// Brand Section //
 
-$('.brand-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: false,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    rtl: false,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
+        // Testimonial Section //
+
+        $('.testimonial-slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+        $('.testimonial-prev-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.testimonial-slider .slick-prev').trigger('click');
+        });
+        $('.testimonial-next-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.testimonial-slider .slick-next').trigger('click');
+        });
+
+
+
+
+        // Team sections //
+
+        $('.team-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+        $('.team-prev-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.team-slider .slick-prev').trigger('click');
+        });
+        $('.team-next-btn').on('click', function (e) {
+            e.preventDefault();
+            $('.team-slider .slick-next').trigger('click');
+        });
+
+
+        // Brand Section //
+
+        $('.brand-slider').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            arrows: false,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            rtl: false,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+    });
+})(jQuery);
